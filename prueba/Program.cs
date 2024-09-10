54d4dd48d58859993ef342b6ea552359acbdbc80
Gines Granados <ginesgb@outlook.es>
2024-09-10 14:01:23 +0200

    mas datos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -6,3 +6,11 @@ using System.Drawing;
 Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
