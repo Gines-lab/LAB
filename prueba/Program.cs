916a9ed0e0b76fec3d4811ec9e581a4cb37a914e
Gines Granados <ginesgb@outlook.es>
2024-09-11 13:43:56 +0200

    Mas datos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -20,8 +20,8 @@ Console.WriteLine(true);
 
 
 string name = "Bob";
-int messages = 3;
 decimal temperature = 34.4m;
+string messages = "tas";
 
 Console.Write("Hello, ");
 Console.Write(name);
@@ -32,9 +32,27 @@ Console.Write(temperature);
 Console.WriteLine(" celsius.");
 
 
-Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
 // To generate Japanese invoices:
 // Nihon no seikyū-sho o seisei suru ni wa:
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 // User command to run an application
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+string message = $"{greeting} {firstName}!";
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
