a57de305c3eb49a0fb83f3f8cdd7e3b970088769
Gines Granados <ginesgb@outlook.es>
2024-09-11 11:38:32 +0200

    temperatura

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -15,5 +15,17 @@ Console.WriteLine(123);
 Console.WriteLine("true");
 Console.WriteLine(true);
 
-string firstName = "Bob";
-Console.WriteLine(firstName);
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.WriteLine(" celsius.");
+
+Console.WriteLine("Hello, " + name + "! You have " + messages + " messages in your inbox. The temperature is " + temperature);
