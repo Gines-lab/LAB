d6f278b0185d430d52300e4e0cff44fbe5cf4a22
Gines Granados <ginesgb@outlook.es>
2024-09-11 13:10:18 +0200

    idiomas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
+using System.Text;
 
 
 // Texto o comentario es este simbolo
@@ -28,4 +29,10 @@ Console.Write(" messages in your inbox. The temperature is ");
 Console.Write(temperature);
 Console.WriteLine(" celsius.");
 
-Console.WriteLine("Hello, " + name + "! You have " + messages + " messages in your inbox. The temperature is " + temperature +" celsius.");
+
+Console.OutputEncoding = Encoding.UTF8;
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
