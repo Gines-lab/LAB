a0798b225cd37e34970197c737a306045c565d0b
Gines Granados <ginesgb@outlook.es>
2024-09-12 11:45:44 +0200

    Matematicas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -64,3 +64,13 @@ Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+int firstNumber = 10;
+int secondNumber = 7;
+
+Console.Clear();
+Console.WriteLine( "Resultado de suma  " + (firstNumber + secondNumber));
+Console.WriteLine( "Resultado de resta  " + (firstNumber - secondNumber));
+Console.WriteLine( "Resultado de multiplicar  " + (firstNumber * secondNumber));
+Console.WriteLine( "Resultado de dividir  " + (firstNumber / secondNumber));
+Console.WriteLine( "Resultado de decimal  " + (firstNumber /(decimal) secondNumber));
