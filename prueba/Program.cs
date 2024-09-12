18104564167781ed7abb427abcbfb088ce4ed3fa
Gines Granados <ginesgb@outlook.es>
2024-09-12 13:45:28 +0200

    Notas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -68,9 +68,9 @@ Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 // Ejercicio de Matematicas
 Console.Clear();
-Console.WriteLine(Environment.NewLine+"##################################################");
-Console.WriteLine("##########   Ejercicio de Matematicas  ###########");
-Console.WriteLine("##################################################");
+Console.WriteLine(Environment.NewLine+"############################################################");
+Console.WriteLine("##########   Ejercicio de Matematicas  #####################");
+Console.WriteLine("############################################################");
 int firstNumber = 10;
 int secondNumber = 7;
 
@@ -83,10 +83,54 @@ Console.WriteLine( "Resultado de decimal  " + (firstNumber /(decimal) secondNumb
 // Temperatura
 int fahrenheit = 94;
 decimal celsius = (fahrenheit - 32m) * (5m / 9m);
-Console.WriteLine(Environment.NewLine+"##################################################");
-Console.WriteLine("##########   Ejercicio de temperatura  ###########");
-Console.WriteLine("##################################################");
+Console.WriteLine(Environment.NewLine+"############################################################");
+Console.WriteLine("##########   Ejercicio de temperatura  #####################");
+Console.WriteLine("############################################################");
 Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
+
+// Notas de estudios
+Console.WriteLine(Environment.NewLine+"############################################################");
+Console.WriteLine("##########   Notas de estudiantes      #####################");
+Console.WriteLine("############################################################");
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
+decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
+decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
+decimal jeongScore = (decimal) jeongSum / currentAssignments;
+
+Console.WriteLine("Sophia: " + sophiaScore + " A");
+Console.WriteLine("Nicolas: " + nicolasScore + " B");
+Console.WriteLine("Zahirah: " + zahirahScore + " B");
+Console.WriteLine("Jeong: " + jeongScore + " A");
 Console.WriteLine(Environment.NewLine+"");
-
-
