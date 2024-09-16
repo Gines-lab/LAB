f3f02ec76496b452867137ccffd10164d7e642fd
Gines Granados <ginesgb@outlook.es>
2024-09-16 11:12:01 +0200

    Secuencias If

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,197 +1,27 @@
-﻿using System.Text;
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-// See https://aka.ms/new-console-template for more information
-using System.Drawing;
-
-
-
-// Texto o comentario es este simbolo
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
-
-Console.WriteLine("123");
-Console.WriteLine(123);
-
-Console.WriteLine("true");
-Console.WriteLine(true);
-
-
-string name = "Bob";
-decimal temperature = 34.4m;
-string messages = "tas";
-
-Console.Write("Hello, ");
-Console.Write(name);
-Console.Write("! You have ");
-Console.Write(messages);
-Console.Write(" messages in your inbox. The temperature is ");
-Console.Write(temperature);
-Console.WriteLine(" celsius.");
-
-
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.Write(@"c:\invoices");
-
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
-
-
-string firstName = "Bob";
-string greeting = "Hello";
-Console.WriteLine(greeting + " " + firstName + "!");
-string message = $"{greeting} {firstName}!";
-
-int version = 11;
-string updateText = "Update to Windows";
-Console.WriteLine($"{updateText} {version}!");
-
-string projectName = "First-Project";
-Console.WriteLine($@"C:\Output\{projectName}\Data");
-
-string projectName1 = "ACME";
-string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
-Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
-
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
-Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
-
-
-// Ejercicio de Matematicas
-Console.Clear();
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Ejercicio de Matematicas  #####################");
-Console.WriteLine("############################################################");
-int firstNumber = 10;
-int secondNumber = 7;
-
-Console.WriteLine("Resultado de suma  " + (firstNumber + secondNumber));
-Console.WriteLine("Resultado de resta  " + (firstNumber - secondNumber));
-Console.WriteLine("Resultado de multiplicar  " + (firstNumber * secondNumber));
-Console.WriteLine("Resultado de dividir  " + (firstNumber / secondNumber));
-Console.WriteLine("Resultado de decimal  " + (firstNumber / (decimal)secondNumber));
-
-// Temperatura
-int fahrenheit = 94;
-decimal celsius = (fahrenheit - 32m) * (5m / 9m);
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Ejercicio de temperatura  #####################");
-Console.WriteLine("############################################################");
-Console.WriteLine("The temperature is " + celsius + " Celsius.");
-
-
-// Notas de estudios
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Notas de estudiantes      #####################");
-Console.WriteLine("############################################################");
-int currentAssignments = 5;
-
-int sophia1 = 93;
-int sophia2 = 87;
-int sophia3 = 98;
-int sophia4 = 95;
-int sophia5 = 100;
-
-int nicolas1 = 80;
-int nicolas2 = 83;
-int nicolas3 = 82;
-int nicolas4 = 88;
-int nicolas5 = 85;
-
-int zahirah1 = 84;
-int zahirah2 = 96;
-int zahirah3 = 73;
-int zahirah4 = 85;
-int zahirah5 = 79;
-
-int jeong1 = 90;
-int jeong2 = 92;
-int jeong3 = 98;
-int jeong4 = 100;
-int jeong5 = 97;
-
-int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
-
-decimal sophiaScore = (decimal)sophiaSum / currentAssignments;
-decimal nicolasScore = (decimal)nicolasSum / currentAssignments;
-decimal zahirahScore = (decimal)zahirahSum / currentAssignments;
-decimal jeongScore = (decimal)jeongSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
-Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
-Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
-Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
-
-// ###############################################
-
-// Random
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Random      #####################");
-Console.WriteLine("############################################################");
-
-
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
-
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Metodo      #####################");
-Console.WriteLine("############################################################");
-
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
-largerValue = Math.Max(firstValue, secondValue);
-Console.WriteLine(largerValue);
-
-
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   IF   Juegon de dados  #####################");
-Console.WriteLine("############################################################");
-
-
-Random dice2 = new Random();
-
-int roll1 = dice2.Next(1, 7);
-int roll2 = dice2.Next(1, 7);
-int roll3 = dice2.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if (total > 10)
+if (daysUntilExpiration == 0)
 {
-    Console.WriteLine("Tu ganas!");
+    Console.WriteLine("Your subscription has expired.");
 }
-else
+else if (daysUntilExpiration == 1)
 {
-    Console.WriteLine("Lo siento, tu pierdes.");
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-
-Console.WriteLine(Environment.NewLine + "############################################################");
-Console.WriteLine("##########   Zorra   #####################");
-Console.WriteLine("############################################################");
-
-string message3 = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message3.Contains("fox"))
+if (discountPercentage > 0)
 {
-    Console.WriteLine("What does the fox say?");
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
