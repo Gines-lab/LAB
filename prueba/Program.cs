e54df182f8b5094db95aed13cdb9aec92be59b55
Gines Granados <ginesgb@outlook.es>
2024-09-19 13:49:36 +0200

    Frase al reves

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,12 @@
-﻿string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
+﻿using System;
+using System.Linq;
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Paso 1 y 3 combinados: Dividimos el string, invertimos cada palabra y las volvemos a juntar en un solo paso
+string result = string.Join(" ", pangram
+                           .Split(' ') // Dividimos la cadena en palabras
+                           .Select(word => new string(word.Reverse().ToArray()))); // Invertimos cada palabra
+
+// Paso 4: Mostramos el resultado
 Console.WriteLine(result);
